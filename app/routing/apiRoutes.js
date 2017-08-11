405849fa2f08fc9224ee7b807a81c9f38b5b5e4a
Heather Sosa <heatherlsosa@gmail.com>
2017-08-11 09:34:58 -0600

    checking for match

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,23 +1,45 @@
-var friendsData = require("../data/friends");
+var friends = require("../data/friends");
 
 module.exports = function(app) {
 	app.get("/api/friends", function(req, res) {
-    res.json(friendsData);
+    res.json(friends);
+
   });
 
-  app.post("/api/friends", function(req, res) {
-      friendsData.unshift(req.body); //unshift adds to the start of array
-      res.json(true);
+app.post('/api/friends', function(req,res) {
 
-      /* PSUEDO CODE
- 	
-	  compare currentUser's answers to EACH user already in the friendsArray
-	  add up differences in answer values
+    var difference = 1000 //MAKE THIS LARGEST TO SO THE difference WILL ALWAYS BE HIGHER THAN totalDifference
+    var matchName = "";
+    var matchPhoto = "";
 
-	  matchedFriend = the user with the LEAST difference in answer values
+    var userData = req.body;
 
+    for (var i = 0; i < friends.length; i++) {
+        var matchScores = [];
+        var totalDifference = 0;
 
+        for (var j = 0; j < 10; j++) {
+            matchScores.push(Math.abs(parseInt(userData.scores[j]) - parseInt(friends[i].scores[j])));
+            
+            totalDifference = matchScores.reduce( function(total, amount){
+                return total + amount
+            });
+            
+        }
+        // console.log(friends[i].name + ": " + totalDifference);
 
-      */
-  });
+        if (totalDifference < difference) {
+          difference = totalDifference;
+          matchName = friends[i].name;
+          matchPhoto = friends[i].photo;
+        }
+        
+    }
+//Once the cycle is complete, the friend with the least difference will remain, and that data will be sent as a json object back to the client
+    res.json({name: matchName, photo: matchPhoto});
+
+    //Lastly, push new User's data into the friends array
+    friends.push(userData);
+});
+
 }
