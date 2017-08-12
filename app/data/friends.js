f93b95935f2e7b6d74dc90f6d414e86511b00090
Heatherly <heatherlsosa@gmail.com>
2017-08-12 15:03:38 -0600

    new links to images

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,19 +3,19 @@ var friends = [
   {
     name: "Ahmed",
     photo:
-      "https://cdn.pixabay.com/photo/2017/07/07/19/42/man-2482464_960_720.jpg",
+      "http://maxpixel.freegreatpicture.com/photo-1885144",
     scores: [1, 2, 4, 2, 2, 3, 1, 1, 5, 5]
   },
   {
     name: "John",
     photo:
-      "https://cdn.pixabay.com/photo/2017/05/22/19/06/male-2334963_960_720.jpg",
+      "http://maxpixel.freegreatpicture.com/photo-1690965",
     scores: [5, 4, 3, 3, 4, 2, 1, 1, 3, 4]
   },
   {
     name: "Jennifer",
     photo:
-      "https://cdn.pixabay.com/photo/2015/01/08/15/46/woman-593141_960_720.jpg",
+      "http://maxpixel.freegreatpicture.com/photo-1274361",
     scores: [2, 2, 3, 4, 2, 1, 1, 1, 5, 5]
   }
 ];
